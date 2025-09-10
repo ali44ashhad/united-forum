0f5b39294c77312d7e75382c555a8dfd584e6ac3
ali44ashhad <ashhadalialvi36@gmail.com>
2025-09-10 15:09:48 +0530

    test

--- models/Thread.js
+++ models/Thread.js
@@ -1,4 +1,40 @@
+// const mongoose = require("mongoose");
+
+// const threadSchema = new mongoose.Schema(
+//   {
+//     title: {
+//       type: String,
+//       required: [true, "Title is required"],
+//       trim: true,
+//       maxlength: [200, "Title cannot exceed 200 characters"],
+//     },
+//     content: {
+//       type: String,
+//       required: [true, "Content is required"],
+//     },
+//     author: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: [true, "Author is required"],
+//     },
+//     views: {
+//       type: Number,
+//       default: 0,
+//     },
+//     replies: {
+//       type: Number,
+//       default: 0,
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// module.exports = mongoose.model("Thread", threadSchema);
+
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const threadSchema = new mongoose.Schema(
   {
@@ -25,10 +61,26 @@ const threadSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    slug: {
+      type: String,
+      unique: true,
+    },
   },
   {
     timestamps: true,
   }
 );
+
+// Pre-save middleware to generate slug
+threadSchema.pre("save", function (next) {
+  if (this.isModified("title") || this.isNew) {
+    this.slug = slugify(this.title, {
+      lower: true,
+      strict: true,
+      remove: /[*+~.()'"!:@]/g,
+    });
+  }
+  next();
+});
 
 module.exports = mongoose.model("Thread", threadSchema);
--- routes/threads.js
+++ routes/threads.js
@@ -1,7 +1,28 @@
+// const express = require("express");
+// const {
+//   getThreads,
+//   getThread,
+//   createThread,
+// } = require("../controllers/threadController");
+// const { protect } = require("../middleware/auth");
+// const {
+//   validateThread,
+//   handleValidationErrors,
+// } = require("../middleware/validation");
+
+// const router = express.Router();
+
+// router.get("/", getThreads);
+// router.get("/:id", getThread);
+// router.post("/", protect, validateThread, handleValidationErrors, createThread);
+
+// module.exports = router;
+
 const express = require("express");
 const {
   getThreads,
   getThread,
+  getThreadBySlug,
   createThread,
 } = require("../controllers/threadController");
 const { protect } = require("../middleware/auth");
@@ -13,7 +34,8 @@ const {
 const router = express.Router();
 
 router.get("/", getThreads);
-router.get("/:id", getThread);
+router.get("/:id", getThread); // For ID-based access (optional)
+router.get("/slug/:slug", getThreadBySlug); // New endpoint for slug-based access
 router.post("/", protect, validateThread, handleValidationErrors, createThread);
 
 module.exports = router;
